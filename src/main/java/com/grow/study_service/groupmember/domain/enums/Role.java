84b7552f3557509ec6bee719e440b763f5bee5bc
sun <suunn001@gmail.com>
2025-08-07 13:24:05 +0900

    :sparkles: feat: 역할 enum 에 상태를 나타내기 위한 설명 코드 추가

--- src/main/java/com/grow/study_service/groupmember/domain/enums/Role.java
+++ src/main/java/com/grow/study_service/groupmember/domain/enums/Role.java
@@ -1,5 +1,13 @@
 package com.grow.study_service.groupmember.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Role {
-	LEADER, MEMBER
+	LEADER("리더"), // 그룹 리더 (운영자)
+	MEMBER("멤버"); // 그룹 멤버
+
+	private final String info;
 }
