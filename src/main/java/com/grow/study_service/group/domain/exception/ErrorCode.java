f7b7bfe0459a76bb530c4927cd959b4c1d1b4a72
sun <suunn001@gmail.com>
2025-08-07 13:09:06 +0900

    :sparkles: feat: 에러 코드 추가

--- src/main/java/com/grow/study_service/group/domain/exception/ErrorCode.java
+++ src/main/java/com/grow/study_service/group/domain/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ErrorCode {
 
     GROUP_NAME_IS_EMPTY("400", "그룹 이름은 비어있을 수 없습니다."),
-    GROUP_DESCRIPTION_IS_EMPTY("400", "그룹 설명은 비어있을 수 없습니다." ), ;
+    GROUP_DESCRIPTION_IS_EMPTY("400", "그룹 설명은 비어있을 수 없습니다." ),
+    CATEGORY_IS_EMPTY("400", "카테고리는 비어있을 수 없습니다." );
 
     private final String code;
     private final String message;
