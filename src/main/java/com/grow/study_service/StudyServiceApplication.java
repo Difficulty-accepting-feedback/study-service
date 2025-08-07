8b64f7e5f9e15876fa4de74ae9ccbb14801397cd
sun <suunn001@gmail.com>
2025-08-07 16:26:07 +0900

    :memo: build: 웹소켓 설정 추가 + 애노테이션 적용

--- src/main/java/com/grow/study_service/StudyServiceApplication.java
+++ src/main/java/com/grow/study_service/StudyServiceApplication.java
@@ -2,7 +2,9 @@ package com.grow.study_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
+@EnableWebSocket
 @SpringBootApplication
 public class StudyServiceApplication {
 
