aacf720860c20f708b675613e968924f51669f79
sun <suunn001@gmail.com>
2025-08-13 20:24:47 +0900

    ✨ feat: NoticeId 를 기반으로 저장된 공지를 찾아오는 레포지토리 로직 추가

--- src/main/java/com/grow/study_service/notice/domain/repository/NoticeRepository.java
+++ src/main/java/com/grow/study_service/notice/domain/repository/NoticeRepository.java
@@ -3,8 +3,10 @@ package com.grow.study_service.notice.domain.repository;
 import com.grow.study_service.notice.domain.model.Notice;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoticeRepository {
-    public void save(Notice notice);
-    public void saveAll(List<Notice> notices);
+    void save(Notice notice);
+    void saveAll(List<Notice> notices);
+    Optional<Notice> findByNoticeId(Long noticeId);
 }
