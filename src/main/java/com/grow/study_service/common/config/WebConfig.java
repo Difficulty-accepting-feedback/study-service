8c8a35cc4a6fc3cfaf65d1e52c51fa95f2d4ad49
sun <suunn001@gmail.com>
2025-08-15 18:12:12 +0900

    ✨ feat: CORS 설정을 위한 WebConfig 추가

---
+++ src/main/java/com/grow/study_service/common/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.grow.study_service.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로 적용
+                .allowedOriginPatterns("*") // 모든 도메인 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600); // 1시간
+    }
+}
