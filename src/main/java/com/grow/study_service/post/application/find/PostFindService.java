9c36903e4377006186606dcbf92c75f4964beae4
sun <suunn001@gmail.com>
2025-08-21 00:02:11 +0900

    ✨ feat: 게시글 전체 조회 서비스 로직 구현

--- src/main/java/com/grow/study_service/post/application/find/PostFindService.java
+++ src/main/java/com/grow/study_service/post/application/find/PostFindService.java
@@ -1,7 +1,11 @@
 package com.grow.study_service.post.application.find;
 
 import com.grow.study_service.post.presentation.dto.response.PostResponse;
+import com.grow.study_service.post.presentation.dto.response.PostSimpleResponse;
+
+import java.util.List;
 
 public interface PostFindService {
     PostResponse findById(Long postId, Long memberId);
+    List<PostSimpleResponse> getPostList(Long memberId, Long boardId);
 }
