fcf877680422fe523477bab7c7fa9a83dc2cc080
sun <suunn001@gmail.com>
2025-08-26 21:17:07 +0900

    ✨ feat: 권한 검증을 위한 레포지토리 코드 구현

--- src/main/java/com/grow/study_service/groupmember/infra/persistence/repository/GroupMemberJpaRepository.java
+++ src/main/java/com/grow/study_service/groupmember/infra/persistence/repository/GroupMemberJpaRepository.java
@@ -2,12 +2,34 @@ package com.grow.study_service.groupmember.infra.persistence.repository;
 
 import java.util.Optional;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.grow.study_service.groupmember.infra.persistence.entity.GroupMemberJpaEntity;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GroupMemberJpaRepository
 	extends JpaRepository<GroupMemberJpaEntity, Long> {
 	Optional<GroupMemberJpaEntity> findByMemberIdAndGroupId(Long memberId, Long groupId);
 	boolean existsByGroupIdAndMemberId(Long groupId, Long memberId);
+
+	/**
+	 * 주어진 postId에 해당하는 포스트가 속한 그룹에 memberId의 멤버가 속해 있는지 확인합니다.
+	 *
+	 * 이 메서드는 GroupMemberJpaEntity, BoardJpaEntity, PostJpaEntity를 조인하여
+	 * 해당 포스트의 그룹 ID를 통해 멤버의 존재 여부를 검사합니다.
+	 * 존재할 경우 true, 그렇지 않을 경우 false를 반환합니다.
+	 *
+	 * @param postId 확인할 포스트의 ID
+	 * @param memberId 확인할 멤버의 ID
+	 * @return 멤버가 해당 포스트의 그룹에 속해 있는지 여부 (true/false)
+	 */
+	@Query("select case when (count(gm) > 0) then true else false end " + // count(gm) > 0 = boolean 타입으로 바꾸기 위해
+			"from GroupMemberJpaEntity gm " +
+			"join BoardJpaEntity b on gm.groupId = b.groupId " +
+			"join PostJpaEntity p on p.boardId = b.id " +
+			"where p.id = :postId and gm.memberId = :memberId") // 주어진 postId와 memberId로 필터링
+	boolean existsByMemberIdAndPostGroup(@Param("postId") Long postId,
+										 @Param("memberId") Long memberId);
+
 }
