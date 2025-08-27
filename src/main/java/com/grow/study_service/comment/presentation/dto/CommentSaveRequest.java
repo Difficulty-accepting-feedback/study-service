4bf9339f77b07dd2a96c0c490bb24b859f9c2f31
sun <suunn001@gmail.com>
2025-08-27 16:40:56 +0900

    ✨ feat: 입력 객체에 검증 애노테이션 추가

--- src/main/java/com/grow/study_service/comment/presentation/dto/CommentSaveRequest.java
+++ src/main/java/com/grow/study_service/comment/presentation/dto/CommentSaveRequest.java
@@ -1,5 +1,8 @@
 package com.grow.study_service.comment.presentation.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +12,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CommentSaveRequest {
 
+    @NotBlank(message = "댓글 내용은 필수입니다.")
+    @Size(min = 1, max = 500, message = "댓글 내용은 1자 이상 500자 이하로 입력해주세요.")
     private String content;
+
+    @Positive(message = "부모 댓글 ID는 양수여야 합니다.") // null 허용, 하지만 값이 있으면 양수만 허용
     private Long parentId; // 부모 댓글 아이디, 없어도 됨 (null 일 경우 첫 댓글)
 }
