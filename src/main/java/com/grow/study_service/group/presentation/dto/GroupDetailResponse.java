0e7fa398798702596b7366ba083529c717629717
sun <suunn001@gmail.com>
2025-08-28 21:48:20 +0900

    ✨ feat: 스터디 그룹 조회 API DTO 추가

---
+++ src/main/java/com/grow/study_service/group/presentation/dto/GroupDetailResponse.java
@@ -0,0 +1,37 @@
+package com.grow.study_service.group.presentation.dto;
+
+import com.grow.study_service.group.domain.enums.Category;
+import com.grow.study_service.group.domain.enums.PersonalityTag;
+import com.grow.study_service.group.domain.enums.SkillTag;
+import com.grow.study_service.group.domain.model.Group;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GroupDetailResponse {
+
+    private Long groupId;
+    private String groupName;
+    private Category category;
+    private String description;
+    private int amount;
+    private int viewCount;
+    private int memberCount;
+    private PersonalityTag personalityTag;
+    private SkillTag skillTag;
+
+    public static GroupDetailResponse of(Group group, int memberCount) {
+        return GroupDetailResponse.builder()
+                .groupId(group.getGroupId())
+                .groupName(group.getName())
+                .category(group.getCategory())
+                .description(group.getDescription())
+                .amount(group.getAmount())
+                .viewCount(group.getViewCount())
+                .memberCount(memberCount)
+                .personalityTag(group.getPersonalityTag())
+                .skillTag(group.getSkillTag())
+                .build();
+    }
+}
