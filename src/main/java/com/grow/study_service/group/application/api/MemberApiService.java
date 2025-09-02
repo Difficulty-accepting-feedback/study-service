945864e1db44e07f4c282777e7e09f6e953232cc
sun <suunn001@gmail.com>
2025-09-02 19:25:40 +0900

    ✨ feat: webflux 를 이용한 api 호출 로직 구현

--- src/main/java/com/grow/study_service/group/application/api/MemberApiService.java
+++ src/main/java/com/grow/study_service/group/application/api/MemberApiService.java
@@ -1,6 +1,11 @@
 package com.grow.study_service.group.application.api;
 
+import reactor.core.publisher.Mono;
+
 import java.util.List;
+import java.util.Map;
+
+import static com.grow.study_service.group.application.api.MemberApiServiceImpl.*;
 
 public interface MemberApiService {
     /**
@@ -12,4 +17,9 @@ public interface MemberApiService {
      * 외부 API 호출로 멤버 이름 조회 (리스트 처리)
      */
     List<String> fetchMemberNames(List<Long> memberIds);
+
+    /**
+     * 가입 요청 정보 확인
+     */
+    Mono<List<MemberInfo>> getNicknameAndScore(List<Long> memberIds);
 }
