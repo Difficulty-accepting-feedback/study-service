30093722637707182abdc5ee2036276f0dba4e6b
sun <suunn001@gmail.com>
2025-09-02 21:16:38 +0900

    ✨ feat: 가입 요청을 수락하고 거절할 수 있는 서비스 로직 구현

--- src/main/java/com/grow/study_service/group/application/join/GroupJoinService.java
+++ src/main/java/com/grow/study_service/group/application/join/GroupJoinService.java
@@ -1,5 +1,6 @@
 package com.grow.study_service.group.application.join;
 
+import com.grow.study_service.group.presentation.dto.join.JoinConfirmRequest;
 import com.grow.study_service.group.presentation.dto.join.JoinInfoResponse;
 import com.grow.study_service.group.presentation.dto.join.JoinRequest;
 
@@ -10,4 +11,6 @@ public interface GroupJoinService {
     void sendJoinRequest(JoinRequest request, Long memberId);
     List<JoinInfoResponse> findGroupIdsByLeaderId(Long memberId);
     List<Long> prepareFindJoinRequest(Long groupId);
+    void acceptJoinRequest(Long memberId, JoinConfirmRequest request);
+    void rejectJoinRequest(Long memberId, JoinConfirmRequest request);
 }
