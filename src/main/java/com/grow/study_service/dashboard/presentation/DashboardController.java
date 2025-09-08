201045446240563345a2728ba7ab811bfbae201e
sun <suunn001@gmail.com>
2025-09-08 20:53:22 +0900

    ✨ feat: 컨트롤러 API 추가
    1. 한줄 공지 API
    2. 출석률 업데이트 API

---
+++ src/main/java/com/grow/study_service/dashboard/presentation/DashboardController.java
@@ -0,0 +1,45 @@
+package com.grow.study_service.dashboard.presentation;
+
+import com.grow.study_service.common.rsdata.RsData;
+import com.grow.study_service.dashboard.application.DashboardService;
+import com.grow.study_service.notice.application.service.NoticeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/dashboard")
+public class DashboardController {
+
+    private final NoticeService noticeService;
+    private final DashboardService dashboardService;
+
+    // 대시보드에 입장 -> 그룹의 출석률을 올려 줄 수 있도록 하는 API
+    @PostMapping("/update-rate/{groupId}")
+    public RsData<Void> incrementAttendanceDays(@RequestHeader("X-Authorization-Id") Long memberId,
+                                                @PathVariable("groupId") Long groupId) {
+
+        dashboardService.incrementAttendanceDays(groupId, memberId);
+
+        return new RsData<>(
+                "200",
+                "출석일 업데이트 완료"
+        );
+    }
+
+    // 대시보드에서 한줄 공지 보여주는 API
+    @GetMapping("/notice-pinned/{groupId}")
+    public RsData<String> getNoticePinned(@RequestHeader("X-Authorization-Id") Long memberId,
+                                          @PathVariable("groupId") Long groupId) {
+
+        String pinnedNotice = noticeService.getPinnedNotice(groupId);
+
+        return new RsData<>(
+                "200",
+                "공지사항 조회 완료",
+                pinnedNotice
+        );
+    }
+
+    // 내 출석률 체크하여 보여 주는 API
+}
