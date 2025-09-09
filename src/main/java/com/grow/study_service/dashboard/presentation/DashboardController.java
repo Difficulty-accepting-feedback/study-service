93a0e27d25161a39450e5ab6183ff76454cc45ab
sun <suunn001@gmail.com>
2025-09-09 20:54:48 +0900

    ✨ feat: 진행률 퍼센트 조회 API 구현

--- src/main/java/com/grow/study_service/dashboard/presentation/DashboardController.java
+++ src/main/java/com/grow/study_service/dashboard/presentation/DashboardController.java
@@ -2,6 +2,7 @@ package com.grow.study_service.dashboard.presentation;
 
 import com.grow.study_service.common.rsdata.RsData;
 import com.grow.study_service.dashboard.application.DashboardService;
+import com.grow.study_service.dashboard.presentation.dto.RemainingDaysDto;
 import com.grow.study_service.notice.application.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -54,4 +55,30 @@ public class DashboardController {
                 attendanceRate
         );
     }
+
+    // 현재 진행률을 나타내는 퍼센트 API
+    @GetMapping("/remaining-percentage/{groupId}")
+    public RsData<Double> getProgressPercentage(@RequestHeader("X-Authorization-Id") Long memberId,
+                                                 @PathVariable("groupId") Long groupId) {
+
+        Double progressPercentage = dashboardService.getProgressPercentage(groupId, memberId);
+
+        return new RsData<>(
+                "200",
+                "진행률 퍼센트 조회 완료",
+                progressPercentage
+        );
+    }
+
+    // == 칸반보드 (To-do list) API ==
+
+    // 투두 조회
+
+    // 투두 등록
+
+    // 투두 상태 변경 (시작 전에서 실행 중 / 완료)
+
+
+
+    // 오늘의 퀴즈
 }
