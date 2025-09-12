51edeaa0d119fe529f9b92f0e56651f10c2fb37f
sun <suunn001@gmail.com>
2025-09-12 21:43:35 +0900

    ✨ feat: 칸반보드 조회 API 추가
    - 한 달 주기 (현재 날짜로부터 한 달) 로 조회 가능
    - 필요하다면 주기를 입력 받아서 조회할 수도 있음 (확장 시 추가)

--- src/main/java/com/grow/study_service/kanbanboard/presentation/controller/KanbanBoardController.java
+++ src/main/java/com/grow/study_service/kanbanboard/presentation/controller/KanbanBoardController.java
@@ -4,8 +4,11 @@ package com.grow.study_service.kanbanboard.presentation.controller;
 import com.grow.study_service.common.rsdata.RsData;
 import com.grow.study_service.kanbanboard.application.KanbanBoardService;
 import com.grow.study_service.kanbanboard.presentation.dto.TodoCreateRequest;
+import com.grow.study_service.kanbanboard.presentation.dto.response.KanbanBoardResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,7 +29,21 @@ public class KanbanBoardController {
         return new RsData<>(
                 "201",
                 "투두 등록 완료",
-                response
+                response // 칸반보드 ID 단건
+        );
+    }
+
+    // 칸반보드 조회 (한 달 주기로 조회)
+    @GetMapping("/todos/{groupId}")
+    public RsData<List<KanbanBoardResponse>> getTodos(@RequestHeader("X-Authorization-Id") Long memberId,
+                                                      @PathVariable("groupId") Long groupId) {
+
+        List<KanbanBoardResponse> responses = kanbanBoardService.getTodos(memberId, groupId);
+
+        return new RsData<>(
+                "200",
+                "투두 조회 완료",
+                responses // 칸반보드 리스트
         );
     }
 }
