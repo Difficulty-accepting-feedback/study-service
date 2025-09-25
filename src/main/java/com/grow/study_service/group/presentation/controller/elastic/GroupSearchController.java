4b461c1d7fcbc325df9ca6a868f16c7833cf91a5
sun <suunn001@gmail.com>
2025-09-25 17:01:44 +0900

    ✨ feat: 검색 API 생성
    - 더 많은 조건이 추가될 경우에 추가 필요

--- src/main/java/com/grow/study_service/group/presentation/controller/elastic/GroupSearchController.java
+++ src/main/java/com/grow/study_service/group/presentation/controller/elastic/GroupSearchController.java
@@ -1,6 +1,9 @@
 package com.grow.study_service.group.presentation.controller.elastic;
 
 import com.grow.study_service.group.application.search.GroupSearchService;
+import com.grow.study_service.group.domain.document.GroupDocument;
+import com.grow.study_service.group.domain.enums.Category;
+import com.grow.study_service.group.domain.enums.SkillTag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +28,17 @@ public class GroupSearchController {
         return ResponseEntity.ok(suggestions);
     }
 
-
     // 검색 API
+    @GetMapping("/search")
+    public ResponseEntity<List<GroupDocument>> getGroupSearch(@RequestParam String query, // 검색할 단어 (name + description + category + skillTag 에서 찾을 것임 (필수))
+                                                              @RequestParam(required = false) Category category,
+                                                              @RequestParam(required = false) SkillTag skillTag,
+                                                              @RequestParam(required = false, defaultValue = "startAt") String sortBy,  // 정렬 기준 (기본: 생성 순) [조회수 순 or 생성 순]
+                                                              @RequestParam(required = false, defaultValue = "desc") String sortOrder,  // 정렬 방향 (asc/desc)
+                                                              @RequestParam(defaultValue = "1") int page,
+                                                              @RequestParam(defaultValue = "10") int size) {
+
+        List<GroupDocument> groupDocuments = searchService.searchGroup(query, category, skillTag, sortBy, sortOrder, page, size);
+        return ResponseEntity.ok(groupDocuments);
+    }
 }
