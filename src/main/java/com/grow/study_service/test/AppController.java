8d703d4840beb1093db2f22130a6cf0804e9ea02
sun <suunn001@gmail.com>
2025-10-02 22:08:34 +0900

    ✅ test: 연결 테스트 용도 컨트롤러 생성

---
+++ src/main/java/com/grow/study_service/test/AppController.java
@@ -0,0 +1,15 @@
+package com.grow.study_service.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AppController {
+
+    @GetMapping
+    public String test() {
+        return "스터디 서버 테스트 연결 - 성공";
+    }
+}
